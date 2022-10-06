788fa12db4010f5e41b0ddef90c404f3b2905609
Michal Pilichowski <48260404+pilichm@users.noreply.github.com>
2022-10-06 18:57:23 +0200

    Update RootWindow.py
    Added method displaying window with mock data for testing of positioninig of all elements.

--- RootWindow.py
+++ RootWindow.py
@@ -26,6 +26,13 @@ def resize_all_labels(labels):
 
 
 # Class for managing application windows.
+def prepare_label_with_image(image_name, image_size):
+    image = Image.open(image_name)
+    image = image.resize(image_size, Image.ANTIALIAS)
+    tkImage = ImageTk.PhotoImage(image)
+    return tk.Label(image=tkImage, anchor='w')
+
+
 class RootWindow:
 
     def __init__(self):
@@ -128,7 +135,6 @@ class RootWindow:
         self.root = tk.Tk()
         self.root.title(self.game.name)
 
-
         # Download cover image.
         self.download_image("cover.png", self.game.cover_url)
 
@@ -149,12 +155,11 @@ class RootWindow:
         for index in range(3):
             screenshotImage = Image.open(f"screenshot_{index}.png")
             screenshotImage = screenshotImage.resize((200, 100), Image.ANTIALIAS)
-            sLabel = tk.Label(image=ImageTk.PhotoImage(screenshotImage))
+            sLabel = tk.Label(image=ImageTk.PhotoImage(screenshotImage), anchor='w')
             screenshot_labels.append(sLabel)
             screenshot_labels[index].grid(row=1, column=index)
 
         self.root.mainloop()
-
 
     # Downloads images from submitted url.
     def download_image(self, img_name, img_url):
@@ -168,6 +173,48 @@ class RootWindow:
     def download_three_random_screenshots(self):
         for index, img_url in enumerate(random.sample(self.game.screenshots, 3)):
             self.download_image(f"screenshot_{index}.png", img_url)
+
+    # Function for testing positioning of elements in window.
+    def display_window_with_mock_data(self):
+        self.root = tk.Tk()
+        self.root.title("mock")
+        screenshot_size = (250, 150)
+
+        # Display cover image.
+        coverImage = Image.open("cover.png")
+        coverImage = coverImage.resize((200, 300), Image.ANTIALIAS)
+        tkImage = ImageTk.PhotoImage(coverImage)
+        label = tk.Label(image=tkImage, anchor='w')
+        label.grid(row=0, column=0, sticky=tk.W, pady=4, columnspan=2, rowspan=3)
+
+        gameNameLabel = tk.Label(self.root, width=50, text="Mass Effect 2", anchor='n')
+        gameNameLabel.grid(row=0, column=1, sticky=tk.W, pady=4)
+
+        gameDescriptionLabel = tk.Label(self.root, width=50, wraplength=250, text="Are you prepared to lose everything to save the galaxy? You'll need to be, Commander Shephard. It's time to bring together your greatest allies and recruit the galaxy's fighting elite to continue the resistance against the invading Reapers. So steel yourself, because this is an astronomical mission where sacrifices must be made. You'll face tougher choices and new, deadlier enemies. Arm yourself and prepare for an unforgettable intergalactic adventure.", anchor='n')
+        gameDescriptionLabel.grid(row=1, column=1, sticky=tk.W, pady=4)
+
+        gameGenresLabel = tk.Label(self.root, width=50, text="Genre: Adventure, Role-playing (RPG), Shooter, Simulator", anchor='n')
+        gameGenresLabel.grid(row=2, column=1, sticky=tk.W, pady=4)
+
+        screenshot_0 = Image.open("screenshot_0.png")
+        screenshot_0 = screenshot_0.resize(screenshot_size, Image.ANTIALIAS)
+        sc_0_image = ImageTk.PhotoImage(screenshot_0)
+        sc_0_label = tk.Label(image=sc_0_image, anchor='w')
+        sc_0_label.grid(row=3, column=0, sticky=tk.W, pady=4, columnspan=1, rowspan=1)
+
+        screenshot_1 = Image.open("screenshot_1.png")
+        screenshot_1 = screenshot_1.resize(screenshot_size, Image.ANTIALIAS)
+        sc_1_image = ImageTk.PhotoImage(screenshot_1)
+        sc_1_label = tk.Label(image=sc_1_image, anchor='c')
+        sc_1_label.grid(row=3, column=1, sticky=tk.S, pady=4, columnspan=1, rowspan=1)
+
+        screenshot_2 = Image.open("screenshot_2.png")
+        screenshot_2 = screenshot_2.resize(screenshot_size, Image.ANTIALIAS)
+        sc_2_image = ImageTk.PhotoImage(screenshot_2)
+        sc_2_label = tk.Label(image=sc_2_image, anchor='w')
+        sc_2_label.grid(row=3, column=2, sticky=tk.W, pady=4, columnspan=1, rowspan=1)
+
+        self.root.mainloop()
 
     # Default method for starting app, displays windows in correct order.
     def run(self):
